972a1e8c1bbccdd8247d90ce92b225739f7d49f9
T-Shikinami <shikinami@grev.co.jp>
2022-10-20 17:03:01 +0900

    リベースを反映

--- Source/PLATEAUEditor/Private/ExtentEditor/SPLATEAUExtentEditorViewport.cpp
+++ Source/PLATEAUEditor/Private/ExtentEditor/SPLATEAUExtentEditorViewport.cpp
@@ -8,27 +8,24 @@
 #include "SSubobjectEditor.h"
 #include "Slate/SceneViewport.h"
 #include "BlueprintEditorSettings.h"
-#include "SlateOptMacros.h"
+#include "PLATEAUExtentEditor.h"
+#include "ExtentEditor/MeshCodeGizmoComponent.h"
 
+#include "plateau/udx/mesh_code.h"
 #include "plateau/udx/udx_file_collection.h"
-
-
-#define LOCTEXT_NAMESPACE "SPLATEAUExtentEditorViewport"
 
 SPLATEAUExtentEditorViewport::SPLATEAUExtentEditorViewport()
     : PreviewScene(MakeShareable(new FAdvancedPreviewScene(FPreviewScene::ConstructionValues()))) {}
 
-SPLATEAUExtentEditorViewport::~SPLATEAUExtentEditorViewport() {
-    if (ViewportClient.IsValid()) {
-        ViewportClient->Viewport = nullptr;
-    }
-}
 
 void SPLATEAUExtentEditorViewport::Construct(const FArguments& InArgs) {
+    bIsActiveTimerRegistered = false;
+
+    // Save off the Blueprint editor reference, we'll need this later
     ExtentEditorPtr = InArgs._ExtentEditor;
 
     SEditorViewport::Construct(SEditorViewport::FArguments());
-
+    
     if (ViewportClient.IsValid()) {
         UWorld* World = ViewportClient->GetPreviewScene()->GetWorld();
         if (World != nullptr) {
@@ -49,14 +46,41 @@ void SPLATEAUExtentEditorViewport::Construct(const FArguments& InArgs) {
         if (FileCollection->getMeshCodes().size() == 0)
             return;
 
-        auto GeoReference = ExtentEditorPtr.Pin()->GetGeoReference();
-        const auto RawCenterPoint = FileCollection->calculateCenterPoint(GeoReference.GetData());
-        GeoReference.ReferencePoint.X = RawCenterPoint.x;
-        GeoReference.ReferencePoint.Y = RawCenterPoint.y;
-        GeoReference.ReferencePoint.Z = RawCenterPoint.z;
-        ExtentEditorPtr.Pin()->SetGeoReference(GeoReference);
+        ViewportClient->InitCamera();
 
-        ViewportClient->Initialize(*FileCollection);
+        plateau::geometry::GeoReference GeoReference(9, TVec3d(0, 0, 0), 1, plateau::geometry::CoordinateSystem::NWU);
+        const auto ReferencePoint = GeoReference.project(FileCollection->getMeshCodes().begin()->getExtent().min);
+        GeoReference.setReferencePoint(ReferencePoint);
+
+        for (const auto& MeshCode : FileCollection->getMeshCodes()) {
+            const auto MeshCodeGizmo = NewObject<UMeshCodeGizmoComponent>();
+            MeshCodeGizmo->Init(MeshCode, GeoReference);
+            ViewportClient->GetPreviewScene()->AddComponent(MeshCodeGizmo, FTransform::Identity);
+        }
+    }
+
+    UEditorEngine* Editor = (UEditorEngine*)GEngine;
+    PreviewFeatureLevelChangedHandle = Editor->OnPreviewFeatureLevelChanged().AddLambda([this](ERHIFeatureLevel::Type NewFeatureLevel) {
+        if (ViewportClient.IsValid()) {
+            UWorld* World = ViewportClient->GetPreviewScene()->GetWorld();
+            if (World != nullptr) {
+                World->ChangeFeatureLevel(NewFeatureLevel);
+
+                RequestRefresh(false);
+            }
+        }
+        });
+    
+    RequestRefresh(true);
+}
+
+SPLATEAUExtentEditorViewport::~SPLATEAUExtentEditorViewport() {
+    UEditorEngine* Editor = (UEditorEngine*)GEngine;
+    Editor->OnPreviewFeatureLevelChanged().Remove(PreviewFeatureLevelChangedHandle);
+
+    if (ViewportClient.IsValid()) {
+        // Reset this to ensure it's no longer in use after destruction
+        ViewportClient->Viewport = NULL;
     }
 }
 
@@ -66,12 +90,9 @@ bool SPLATEAUExtentEditorViewport::IsVisible() const {
 }
 
 TSharedRef<FEditorViewportClient> SPLATEAUExtentEditorViewport::MakeEditorViewportClient() {
+
     // Construct a new viewport client instance.
-    ViewportClient = MakeShareable(
-        new FPLATEAUExtentEditorViewportClient(
-            ExtentEditorPtr,
-            SharedThis(this),
-            PreviewScene.ToSharedRef()));
+    ViewportClient = MakeShareable(new FPLATEAUExtentEditorViewportClient(SharedThis(this), PreviewScene.ToSharedRef()));
     ViewportClient->SetRealtime(true);
     ViewportClient->bSetListenerPosition = false;
     ViewportClient->VisibilityDelegate.BindSP(this, &SPLATEAUExtentEditorViewport::IsVisible);
@@ -79,75 +100,80 @@ TSharedRef<FEditorViewportClient> SPLATEAUExtentEditorViewport::MakeEditorViewpo
     return ViewportClient.ToSharedRef();
 }
 
-BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
 void SPLATEAUExtentEditorViewport::PopulateViewportOverlays(TSharedRef<class SOverlay> Overlay) {
     SEditorViewport::PopulateViewportOverlays(Overlay);
 
     // add the feature level display widget
     Overlay->AddSlot()
-        .VAlign(VAlign_Top)
-        .HAlign(HAlign_Left)
+        .VAlign(VAlign_Bottom)
+        .HAlign(HAlign_Right)
         .Padding(5.0f)
         [
-            SNew(SBorder)
-            .BorderImage(FAppStyle::Get().GetBrush("FloatingBorder"))
-        .Padding(4.0f)
-        [
-            SNew(SVerticalBox)
-            + SVerticalBox::Slot()
-        .AutoHeight()
-        .Padding(FMargin(0, 0, 0, 15))
-        [
-            SNew(SButton)
-            .VAlign(VAlign_Center)
-        .ForegroundColor(FColor::White)
-        .ButtonColorAndOpacity(FColor(10, 90, 80, 255))
-        .OnClicked_Lambda(
-            [this]() {
-                if (GetOwnerTab())
-                    GetOwnerTab()->RequestCloseTab();
-                return FReply::Handled();
-            })
-        .Content()
-                [
-                    SNew(STextBlock)
-                    .Justification(ETextJustify::Center)
-                .Margin(FMargin(0, 5, 0, 5))
-                .Text(LOCTEXT("Cancel Button", "キャンセル"))
-                ]
-        ]
-    + SVerticalBox::Slot()
-        .AutoHeight()
-        .Padding(FMargin(0, 0, 0, 15))
-        [
-            SNew(SButton)
-            .VAlign(VAlign_Center)
-        .ForegroundColor(FColor::White)
-        .ButtonColorAndOpacity(FColor(10, 90, 80, 255))
-        .OnClicked_Lambda(
-            [this]() {
-                const auto Extent = ViewportClient->GetExtent();
-                ExtentEditorPtr.Pin()->SetExtent(Extent);
-                ExtentEditorPtr.Pin()->HandleClickOK();
-
-                if (GetOwnerTab())
-                    GetOwnerTab()->RequestCloseTab();
-                return FReply::Handled();
-            })
-        .Content()
-                [
-                    SNew(STextBlock)
-                    .Justification(ETextJustify::Center)
-                .Margin(FMargin(0, 5, 0, 5))
-                .Text(LOCTEXT("OK Button", "決定"))
-                ]
-        ]
-        ]
+            BuildFeatureLevelWidget()
         ];
 }
-END_SLATE_FUNCTION_BUILD_OPTIMIZATION
 
 void SPLATEAUExtentEditorViewport::BindCommands() {}
+
+void SPLATEAUExtentEditorViewport::Invalidate() {
+    //ViewportClient->Invalidate();
+}
+
+void SPLATEAUExtentEditorViewport::ToggleIsSimulateEnabled() {
+    // Make the viewport visible if the simulation is starting.
+    //if (!ViewportClient->GetIsSimulateEnabled()) {
+    //    if (GetDefault<UBlueprintEditorSettings>()->bShowViewportOnSimulate) {
+    //        BlueprintEditorPtr.Pin()->GetTabManager()->TryInvokeTab(FBlueprintEditorTabs::SCSViewportID);
+    //    }
+    //}
+
+    //ViewportClient->ToggleIsSimulateEnabled();
+}
+
+void SPLATEAUExtentEditorViewport::EnablePreview(bool bEnable) {
+    //const FText SystemDisplayName = NSLOCTEXT("BlueprintEditor", "RealtimeOverrideMessage_Blueprints", "the active blueprint mode");
+    //if (bEnable) {
+    //    // Restore the previously-saved realtime setting
+    //    ViewportClient->RemoveRealtimeOverride(SystemDisplayName);
+    //} else {
+    //    // Disable and store the current realtime setting. This will bypass real-time rendering in the preview viewport (see UEditorEngine::UpdateSingleViewportClient).
+    //    const bool bShouldBeRealtime = false;
+    //    ViewportClient->AddRealtimeOverride(bShouldBeRealtime, SystemDisplayName);
+    //}
+}
+
+void SPLATEAUExtentEditorViewport::RequestRefresh(bool bResetCamera, bool bRefreshNow) {
+    //if (bRefreshNow) {
+    //    if (ViewportClient.IsValid()) {
+    //        ViewportClient->InvalidatePreview(bResetCamera);
+    //    }
+    //} else {
+    //    // Defer the update until the next tick. This way we don't accidentally spawn the preview actor in the middle of a transaction, for example.
+    //    if (!bIsActiveTimerRegistered) {
+    //        bIsActiveTimerRegistered = true;
+    //        RegisterActiveTimer(0.f, FWidgetActiveTimerDelegate::CreateSP(this, &SPLATEAUExtentEditorViewport::DeferredUpdatePreview, bResetCamera));
+    //    }
+    //}
+}
+
+void SPLATEAUExtentEditorViewport::OnComponentSelectionChanged() {
+    // When the component selection changes, make sure to invalidate hit proxies to sync with the current selection
+    SceneViewport->Invalidate();
+}
+
+void SPLATEAUExtentEditorViewport::OnFocusViewportToSelection() {
+    //ViewportClient->FocusViewportToSelection();
+}
+
+bool SPLATEAUExtentEditorViewport::ShouldShowViewportCommands() const {
+    // Hide if actively debugging
+    return !GIntraFrameDebuggingGameThread;
+}
+
+bool SPLATEAUExtentEditorViewport::GetIsSimulateEnabled() {
+    // return ViewportClient->GetIsSimulateEnabled();
+    return false;
+}
 
 void SPLATEAUExtentEditorViewport::SetOwnerTab(TSharedRef<SDockTab> Tab) {
     OwnerTab = Tab;
@@ -157,4 +183,18 @@ TSharedPtr<SDockTab> SPLATEAUExtentEditorViewport::GetOwnerTab() const {
     return OwnerTab.Pin();
 }
 
-#undef LOCTEXT_NAMESPACE
+FReply SPLATEAUExtentEditorViewport::OnDrop(const FGeometry& MyGeometry, const FDragDropEvent& DragDropEvent) {
+    //TSharedPtr<SSubobjectEditor> SubobjectEditor = BlueprintEditorPtr.Pin()->GetSubobjectEditor();
+
+    //return SubobjectEditor->TryHandleAssetDragDropOperation(DragDropEvent);
+    return FReply::Handled();
+}
+
+EActiveTimerReturnType SPLATEAUExtentEditorViewport::DeferredUpdatePreview(double InCurrentTime, float InDeltaTime, bool bResetCamera) {
+    if (ViewportClient.IsValid()) {
+        //ViewportClient->InvalidatePreview(bResetCamera);
+    }
+
+    bIsActiveTimerRegistered = false;
+    return EActiveTimerReturnType::Stop;
+}
