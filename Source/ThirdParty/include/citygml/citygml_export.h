eb98a864503acd2f788044ce2ca9ff373afa3bb5
linoal <1321932+linoal@users.noreply.github.com>
2023-04-27 22:25:41 +0900

    citygmlのincludeが古かったので反映

--- Source/ThirdParty/include/citygml/citygml_export.h
+++ Source/ThirdParty/include/citygml/citygml_export.h
@@ -9,10 +9,10 @@
 #  ifndef LIBCITYGML_EXPORT
 #    ifdef citygml_EXPORTS
         /* We are building this library */
-#      define LIBCITYGML_EXPORT __declspec(dllexport)
+#      define LIBCITYGML_EXPORT 
 #    else
         /* We are using this library */
-#      define LIBCITYGML_EXPORT __declspec(dllimport)
+#      define LIBCITYGML_EXPORT 
 #    endif
 #  endif
 
