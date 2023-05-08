a63e6fe9d03f9557403a6c8ae937175206b83a41
COREI5\taka <seventhdim@gmail.com>
2023-05-08 15:47:35 +0900

    Basemapの変数名を修正

--- Source/PLATEAUEditor/Private/PLATEAUBasemap.h
+++ Source/PLATEAUEditor/Private/PLATEAUBasemap.h
@@ -58,7 +58,7 @@ public:
         return TileComponent;
     }
 
-    void StartLoading(const FPLATEAUTileCoordinate& InTileCoordinate, FPipe& Pipe);
+    void StartLoading(const FPLATEAUTileCoordinate& InTileCoordinate, FPipe& VectorTilePipe);
 
 private:
     FCriticalSection CriticalSection;
