9142a95d79cd4d0a5c518c23e4214c762a3eb981
Sacchy <mandw.entertainment@gmail.com>
2023-11-07 21:12:54 +0900

    3Dファイルエクスポート時にファイルの上書き・エクスポート結果の確認ができる (#182)
    * 3Dファイルエクスポート時にファイルの上書き・エクスポート結果の確認ができる
    
    * コンフリクト解消
    
    * UE5.3対応
    
    * 3Dファイルエクスポート時にファイルの上書き・エクスポート結果の確認ができる
    
    ・クラッシュ対応

--- Source/PLATEAUEditorBPLibraries/Private/Export/PLATEAUExportModelBtn.cpp
+++ Source/PLATEAUEditorBPLibraries/Private/Export/PLATEAUExportModelBtn.cpp
@@ -1,7 +1,10 @@
 // Copyright © 2023 Ministry of Land, Infrastructure and Transport
 
 #include "Export/PLATEAUExportModelBtn.h"
+#include "PLATEAUExportSettings.h"
 #include "PLATEAUMeshExporter.h"
+#include "Kismet/KismetSystemLibrary.h"
+#define LOCTEXT_NAMESPACE "UPLATEAUExportModelBtn"
 
 /**
  * @brief モデル出力
@@ -15,6 +18,17 @@
  * @param CoordinateSystem 座標系設定
  */
 void UPLATEAUExportModelBtn::ExportModel(APLATEAUInstancedCityModel* TargetCityModel, const FString& ExportPath, const uint8 FileFormat, const bool bExportAsBinary, const bool bExportHiddenModel, const bool bExportTexture, const uint8 TransformType, const uint8 CoordinateSystem) {
+    const auto& FoundFileArray = plateau::Export::GetFoundFiles(static_cast<EMeshFileFormat>(FileFormat), ExportPath);
+    if (0 < FoundFileArray.Num()) {
+        const FText Title = LOCTEXT("ConfirmOverwriteTitle", "ファイル出力確認");
+        FFormatOrderedArguments Args;
+        Args.Add(FText::FromString(plateau::Export::MeshFileFormatToStr(static_cast<EMeshFileFormat>(FileFormat))));
+        const FText DialogText = FText::Format(LOCTEXT("ConfirmOverwriteDesc", "出力先に{0}ファイルが既に存在しています。\n上書きしますか？"), Args);
+        if (FMessageDialog::Open(EAppMsgType::YesNo, DialogText, Title) == EAppReturnType::No) {
+            return;
+        }
+    }
+    
     MeshExportOptions Options;
     Options.FileFormat = static_cast<EMeshFileFormat>(FileFormat);
     plateau::meshWriter::GltfWriteOptions GltfOptions;
@@ -28,5 +42,25 @@ void UPLATEAUExportModelBtn::ExportModel(APLATEAUInstancedCityModel* TargetCityM
     Options.TransformType = static_cast<EMeshTransformType>(TransformType);
     Options.CoordinateSystem = static_cast<ECoordinateSystem>(CoordinateSystem);
     FPLATEAUMeshExporter MeshExporter;
-    MeshExporter.Export(ExportPath, TargetCityModel, Options);
+
+    if (!FPaths::DirectoryExists(ExportPath)) {
+        FFileManagerGeneric::Get().MakeDirectory(*ExportPath, true);
+    }
+    
+    if (MeshExporter.Export(ExportPath, TargetCityModel, Options)) {
+        const auto OpenDirectlyPath = FString::Format(TEXT("file://{0}"), {ExportPath});
+        if (UKismetSystemLibrary::CanLaunchURL(OpenDirectlyPath)) {
+            UKismetSystemLibrary::LaunchURL(OpenDirectlyPath);
+        } else {
+            const FText Title = LOCTEXT("OpenDirectlyFailureResultTitle", "エラー");
+            const FText DialogText = LOCTEXT("OpenDirectlyFailureResultDesc", "出力先のディレクトリが開けませんでした。");
+            FMessageDialog::Open(EAppMsgType::Ok, DialogText, Title);
+        }
+    } else {
+        const FText Title = LOCTEXT("FileExportFailureResultTitle", "エラー");
+        const FText DialogText = LOCTEXT("FileExportFailureResultDesc", "ファイル出力に失敗しました。");
+        FMessageDialog::Open(EAppMsgType::Ok, DialogText, Title);
+    }
 }
+
+#undef LOCTEXT_NAMESPACE
