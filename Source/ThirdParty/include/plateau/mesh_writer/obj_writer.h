05b47d30518164772573f50bda6a733ec2249f8a
sevendev <taka@7jigen.net>
2024-04-19 14:59:41 +0900

    lib update

--- Source/ThirdParty/include/plateau/mesh_writer/obj_writer.h
+++ Source/ThirdParty/include/plateau/mesh_writer/obj_writer.h
@@ -11,38 +11,6 @@
 
 namespace plateau::meshWriter {
 
-    /// ゲームオブジェクトの位置のスタックです。
-    /// Nodeの親から子へと処理が進むたびpushし、子から親へ処理が戻るたびpopします。
-    /// こうすることで、親から子まで累積的にTransformを適用することができます。
-    class TransformStack {
-    public:
-        void push(const polygonMesh::Transform& transform) {
-            stack_.push_back(transform);
-        }
-
-        polygonMesh::Transform pop() {
-            if (stack_.empty()) {
-                throw std::runtime_error("TransformStack is empty.");
-            }
-            auto transform = stack_.at(stack_.size() - 1);
-            stack_.pop_back();
-            return transform;
-        }
-
-        /// スタック内の全Transformを掛け合わせて積を計算します。
-        polygonMesh::Transform CalcProduct() {
-            auto current = glm::mat4(1.0f);
-            for(int i=stack_.size() - 1; i>=0; i--) {
-                current = stack_.at(i).toGlmMatrix() * current;
-            }
-            return polygonMesh::Transform::fromGlmMatrix(current);
-        }
-
-
-    private:
-        std::vector<polygonMesh::Transform> stack_;
-    };
-
     /// ModelをもとにOBJファイルを書き出すクラスです。
     class LIBPLATEAU_EXPORT ObjWriter {
     public:
--- Source/ThirdParty/include/plateau/polygon_mesh/transform.h
+++ Source/ThirdParty/include/plateau/polygon_mesh/transform.h
@@ -1,31 +1,32 @@
 #pragma once
 
 #include <libplateau_api.h>
-#include <citygml/citygml.h>
-#include <plateau/polygon_mesh/quaternion.h>
-#include <glm/glm.hpp>
+#include <citygml/vecs.hpp>
+#include <memory>
+
+namespace plateau::meshWriter {
+    class TransformStack;
+}
+namespace plateau::polygonMesh {
+    class Quaternion;
+}
+
 
 namespace plateau::polygonMesh {
     /// ModelのNodeが持つトランスフォームです。
     class LIBPLATEAU_EXPORT Transform {
     public:
-        Transform() :
-            Transform(
-                    TVec3d(0.0, 0.0, 0.0),
-                    TVec3d(1.0, 1.0, 1.0),
-                    Quaternion()) {}
+        Transform();
+        Transform(const TVec3d& local_position,
+                  const TVec3d& local_scale,
+                  const Quaternion& local_rotation);
 
-        Transform(const TVec3d& local_position, const TVec3d& local_scale, const Quaternion& local_rotation) :
-        local_position_(local_position),
-        local_scale_(local_scale),
-        local_rotation_(local_rotation){}
-
-        TVec3d getLocalPosition() const { return local_position_; }
-        TVec3d getLocalScale() const {return local_scale_; }
-        Quaternion getLocalRotation() const { return local_rotation_; }
-        void setLocalPosition(TVec3d local_position) { local_position_ = local_position; }
-        void setLocalScale(TVec3d local_scale) { local_scale_ = local_scale; }
-        void setLocalRotation(Quaternion local_rotation) { local_rotation_ = local_rotation; }
+        TVec3d getLocalPosition() const;
+        TVec3d getLocalScale() const;
+        Quaternion getLocalRotation() const;
+        void setLocalPosition(TVec3d local_position);
+        void setLocalScale(TVec3d local_scale);
+        void setLocalRotation(Quaternion local_rotation);
 
         /// GLMライブラリを使って、2つのTransformを合成したTransformを作って返します。
         /// 適用順番は自身→otherです。
@@ -34,15 +35,11 @@ namespace plateau::polygonMesh {
         /// 点に対してTransformを適用した点の座標を返します。
         TVec3d apply(TVec3d point) const;
 
-        /// Transformを、GLMライブラリの変換行列にして返します。
-        glm::mat4x4 toGlmMatrix() const;
-
-        /// GLMライブラリの変換行列からTransformを作って返します。
-        static Transform fromGlmMatrix(glm::mat4x4 matrix);
+        // Implは実質internalのように扱いたいためのfriendです
+        friend class plateau::meshWriter::TransformStack;
 
     private:
-        TVec3d local_position_;
-        TVec3d local_scale_;
-        Quaternion local_rotation_;
+        class Impl;
+        std::shared_ptr<Impl> impl_;
     };
 };
