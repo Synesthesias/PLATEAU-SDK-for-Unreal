edadfecdd39c8d6ab514a0644acc1095b67bf27f
sevendev <taka@7jigen.net>
2024-07-05 09:34:47 +0900

    const value fix

--- Source/PLATEAURuntime/Public/Reconstruct/PLATEAUModelAlignLand.h
+++ Source/PLATEAURuntime/Public/Reconstruct/PLATEAUModelAlignLand.h
@@ -11,11 +11,11 @@ class PLATEAURUNTIME_API FPLATEAUModelAlignLand : public FPLATEAUModelReconstruc
 public:
 
     //HeightMapAligner Parameters
-    const double HeightOffset = 300;
+    const double HeightOffset = 30;
     const float MaxEdgeLength = 400.f; // 高さ合わせのためメッシュを細分化するときの、最大の辺の長さ。だいたい4mくらいが良さそう。
     const int AlphaExpandWidthCartesian = 200; // 逆高さ合わせのアルファの平滑化処理において、不透明部分を広げる幅（直交座標系）
     const int AlphaAveragingWidthCartesian = 200; // 逆高さ合わせのアルファの平滑化処理において、周りの平均を取る幅（直交座標系）
-    const double InvertedHeightOffset = -200.0; // 逆高さ合わせで、土地を対象と比べてどの高さに合わせるか（直交座標系）
+    const double InvertedHeightOffset = -20.0; // 逆高さ合わせで、土地を対象と比べてどの高さに合わせるか（直交座標系）
     const float SkipThresholdOfMapLandDistance = 80.f; // 逆高さ合わせで、土地との距離がこの値以上の箇所は高さ合わせしない（直交座標系）
 
     // 地形にAlignするパッケージリスト
