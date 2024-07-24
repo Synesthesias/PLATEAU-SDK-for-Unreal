46c2a3b0762a333f0f943f8777606bebc880fed2
sevendev <taka@7jigen.net>
2024-07-24 15:46:26 +0900

    lib update

--- Source/ThirdParty/include/plateau/height_map_generator/heightmap_mesh_generator.h
+++ Source/ThirdParty/include/plateau/height_map_generator/heightmap_mesh_generator.h
@@ -2,21 +2,18 @@
 
 #include <libplateau_api.h>
 #include "plateau/height_map_generator/heightmap_types.h"
+#include "plateau/height_map_generator/heightmap_generator.h"
 #include "plateau/polygon_mesh/mesh.h"
 
 namespace plateau::heightMapGenerator {
 
-    class LIBPLATEAU_EXPORT HeightmapMeshGenerator {
+    class LIBPLATEAU_EXPORT HeightmapMeshGenerator : HeightmapGenerator {
     public:
         void generateMeshFromHeightmap(
             plateau::polygonMesh::Mesh& out_mesh, const size_t width,
             const size_t height, const float zScale,
             const HeightMapElemT* data, geometry::CoordinateSystem coordinate,
             TVec3d Min, TVec3d Max, TVec2f UVMin, TVec2f UVMax, const bool invert_mesh);
-
-    private:
-        double getPositionFromPercent(double percent, double min, double max);
-        TVec2d getPositionFromPercent(const TVec2d& percent, const TVec2d& min, const TVec2d& max);
     };
 
-} // namespace plateau::texture
+} // namespace plateau::heightMapGenerator
